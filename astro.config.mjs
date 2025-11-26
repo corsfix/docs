8e1f5b7296cea85cec88f753f32e2a954c9a615e
Reynaldi Chernando <reynaldichernando@gmail.com>
2025-11-26 20:38:59 +0700

    Adjust clarity transient id

--- astro.config.mjs
+++ astro.config.mjs
@@ -74,6 +74,7 @@ export default defineConfig({
         c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
         t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
         y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+        c[a]("identify", (sessionStorage.cid ??= crypto.randomUUID()));
     })(window, document, "clarity", "script", "u3acbobvot");`
         },
         {
